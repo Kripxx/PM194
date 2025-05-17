bd32dde56eb6c4934d1d6160af928f199974bf53
Kripxx <125685085+Kripxx@users.noreply.github.com>
2025-05-16 19:46:30 -0600

    Ejercicio A corregido

---
+++ Practica1/EjercicioA.js
@@ -0,0 +1,21 @@
+//Ejercicio A
+// Corrigue el codigo con buena practicas sin usar var
+
+// var nombre = "Armando";
+// var edad = 25;
+
+// nombre = "Ana Maria";
+
+// var saludo = "Hola" + nombre + " tienes " + edad + " años";
+
+
+//Ejercicio corregido
+let nombre = "Armando";
+
+const edad = 25;
+
+nombre = "Ana Maria";
+
+const saludo = `Hola ${nombre} tienes ${edad} años`;
+
+console.log(saludo);
