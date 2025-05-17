4e3b930c3663c072fa1ed76d7b7be74fcf34c086
Kripxx <125685085+Kripxx@users.noreply.github.com>
2025-05-16 19:49:26 -0600

    EjercicioB corregido

---
+++ Practica1/EjercicioB.js
@@ -0,0 +1,10 @@
+//Convierte esta funcion tradicional a una funcion de flecha
+// function cuadrado(numero){
+//     return numero * numero;
+// }
+
+
+//Funcion de flecha
+const cuadrado = (numero) => numero * numero;
+//Ejecuta la funcion
+console.log(cuadrado(5)); 
