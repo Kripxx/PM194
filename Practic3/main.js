112091b2ede941aa28db9848cd350df34b38c56b
Kripxx <125685085+Kripxx@users.noreply.github.com>
2025-05-21 20:10:34 -0600

    EjercicioA

---
+++ Practic3/main.js
@@ -0,0 +1,8 @@
+//Importa la funcion restar y ejecuta varias pruebas
+import { restar } from './utils.js';
+//Ejecuta la funcion restar con los argumentos 10 y 5
+console.log(restar(10, 5)); // 5
+//Ejecuta la funcion restar con los argumentos 20 y 10
+console.log(restar(20, 10)); // 10
+//Ejecuta la funcion restar con los argumentos 30 y 15
+console.log(restar(30, 15)); // 15
---
+++ Practic3/utils.js
@@ -0,0 +1,4 @@
+//Funcion para restar (a,b) que retorne la resta de a y b
+export function restar(a, b) {
+    return a - b;
+}
