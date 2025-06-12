c83ef2371edd7e3485256dfa8325a729a2b49403
Kripxx <125685085+Kripxx@users.noreply.github.com>
2025-06-11 19:40:12 -0600

    Practica No.4:

--- Practica4/my-black/App.js
+++ Practica4/my-black/App.js
@@ -1,15 +1,39 @@
+/* Zona 1: Importaciones */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+
+const Texto = () => {
+  const [children, setChildren] = useState('Hola Mundo Rnative');
+  const actualizarTexto = () => {
+    setChildren('Hola Mundo Rnative Actualizado');
+  }
+  return (
+    <Text onPress={actualizarTexto}>{children}</Text>
+  );
+};
+
+const Boton = () => {
+  const [children, setChildren] = useState('Hola Mundo Rnative');
+  const actualizarTexto = () => {
+    setChildren('Hola Mundo Rnative Actualizado');
+  }
+  return (
+    <Button title={children} onPress={actualizarTexto} />
+  );
+}
+/* Zona 2 : Main*/
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto>Hola Mundo</Texto>
+      <Boton />
       <StatusBar style="auto" />
     </View>
   );
 }
-
+/* Estetica del screen */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
